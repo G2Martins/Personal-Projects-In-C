32ff48e6052712d12241fb744a0b71adaa774963
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-05-10 00:11:15 -0300

    Create aula1lacos_incrementar.c

---
+++ Laços de Repetição/aula1lacos_incrementar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i = 0;
+
+    while(i <= 10){
+        
+        printf("%d \n",i);
+        i = i + 1; // PODEMOS UTILIZAR TAMBÉM i++ Para incrementer 1 OU --> i += 1. Para Incrementar 3 --> i = i + 3.
+        
+    }
+
+    return 0;
+}
